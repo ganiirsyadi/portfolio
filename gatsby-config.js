d93c2a8bc6b9dce22cde4f0aaead1bfbea890a67
Gani Ilham Irsyadi <ganiilhamirsyadi@gmail.com>
2020-12-24 19:16:49 +0700

    initialize gatsby project

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: "portfolio",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+  ],
+};
